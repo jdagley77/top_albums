86b9b6a32e27e1661cf28405f293accd7a3f9c71
Jack Dagley <jack.dagley77@gmail.com>
2017-06-20 13:13:36 -0700

    Add migration for top albums

--- db/migrate/20170620201118_create_top_albums.rb
+++ db/migrate/20170620201118_create_top_albums.rb
@@ -1,6 +1,8 @@
 class CreateTopAlbums < ActiveRecord::Migration[5.0]
   def change
     create_table :top_albums do |t|
+    	t.integer :user_id
+    	t.integer :album_id
 
       t.timestamps
     end
