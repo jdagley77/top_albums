45df305596955bf19151ff78e29fca17f95646d3
Jack Dagley <jack.dagley77@gmail.com>
2017-06-20 13:31:28 -0700

    Fix associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
 	has_many :top_albums
+	has_many :albums, through: :top_albums
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620195427) do
+ActiveRecord::Schema.define(version: 20170620201118) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "artist"
     t.string   "title"
     t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "top_albums", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
